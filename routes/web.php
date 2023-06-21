2da00590a194e4e1607febfa179a279f829810d8
Alvinalia <133779368+Alvinalia@users.noreply.github.com>
2023-06-21 18:31:03 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,11 @@ use App\Http\Controllers\LabelController;
 use App\Http\Controllers\PenyanyiController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\MusikController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\home2Controller;
-use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LoginControllers;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -24,17 +27,20 @@ use App\Http\Controllers\LoginController;
 */
 
 Route::middleware(['auth'])->group(function(){
-    Route::get('/', [LoginController::class, 'login'])->name('login');
-    Route::post('/login', [LoginController::class, 'login'])->name('login');
-
-    // Route::get('/home2', [home2Controller::class, 'index3'])->middleware('auth');
-    Route::get('/actionlogout', [LoginController::class, 'actionlogout'])->name('logout')->middleware('auth');
+    Route::get('/', function(){
+    return view('/login');
+    });
+    Route::get('/login', [LoginControllers::class, 'login']);
+    Route::get('/register', [LoginControllers::class, 'login']);
+    Route::get('/home', [HomeController::class, 'index']);
+    Route::get('/home2', [home2Controller::class, 'index3']);
+    Route::get('/actionlogout', [LoginController::class, 'actionlogout']);
 
     Route::get('/Penyanyi',[PenyanyiController::class,'index']);
     Route::get('/Genre',[GenreController::class,'index']);
     Route::get('/GenreMusik',[GenreMusikController::class,'index']);
     Route::get('/Label',[LabelController::class,'index']);
-    Route::get('/home',[GenreMusikController::class,'index']);
+    Route::get('/home2',[GenreMusikController::class,'index']);
 
     Route::get('/User',[UserController::class,'index']);
     Route::get('/create',[UserController::class,'create']);
@@ -44,8 +50,6 @@ Route::middleware(['auth'])->group(function(){
     Route::delete('/User/{id}',[UserController::class,'destroy']);
     Route::get('/Musik',[MusikController::class,'index']);
 });
-
-
 
 
 Auth::routes();

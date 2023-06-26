967bdfbec12ec3880aafab3d006c5e0d7aa6f17c
Alvinalia <133779368+Alvinalia@users.noreply.github.com>
2023-06-26 09:45:05 +0700

    Update GenreMusik.blade.php

--- resources/views/GenreMusik.blade.php
+++ resources/views/GenreMusik.blade.php
@@ -1,15 +1,22 @@
 @include('navbarrr')
 @section('content')
 <h2>Genre kalangan Musik</h2>
+@if(Session::has('status'))
+<div class="alert alert-success" role="alert">
+    {{Session::get('message')}}
+</div>
+@endif
 <table class="table">
     <thead>
+        <div class="my-3">
+            <a href="create1" class= "btn btn-dark ml-5">Add Data</a>
+        </div>
         <tr>
             <th>No. </th>
             <th>id</th>
             <th>id_musik</th>
             <th>id_genre</th>
-            <th>Musik id</th>
-            <th>Genre id</th>
+            <th>Action</th>
         </tr>
     </thead>
     <tbody>
@@ -17,10 +24,16 @@
         <tr>
             <td>{{$loop->iteration}}</td>
             <td>{{$data->id}}</td>
-            <td>{{$data->id_musik}}</td><br>
+            <td>{{$data->id_musik}}</td>
             <td>{{$data->id_genre}}</td>
-            <td>{{$data->id}}</td>
-            <td>{{$data->id}}</td>
+            <td>
+            
+            <form class="d-inline" action="/GenreMusik/{{$data->id}}" method="post">
+                    @csrf
+                    @method('delete')
+                    <button type="submit" name="submit" class="btn btn-danger" onclick="return confirm('mau dii apuss nii??')">Delete</button>
+                </form>
+               
             
         </tr>
         @endforeach

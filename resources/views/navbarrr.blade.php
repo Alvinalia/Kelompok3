1ae24526ed863f062512dcb4159cfe99b6c390be
Alvinalia <133779368+Alvinalia@users.noreply.github.com>
2023-06-26 09:49:31 +0700

    Update navbarrr.blade.php

--- resources/views/navbarrr.blade.php
+++ resources/views/navbarrr.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">SONG</a>
+  <a class="navbar-brand" href="/home">SONG</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -17,14 +17,14 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/home"><font color = "blue" > <b> Home </b> </font> <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/Penyanyi">penyanyi</a>
+        <a class="nav-link" href="/Penyanyi"><font color = "purple" > <b> Penyanyi </b> </font> </a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+          <b> More </b> 
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="GenreMusik">genre_musik</a>
@@ -35,10 +35,11 @@
         </div>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+        <a class="nav-link" href="/actionlogout"><font color = "red" > Logout</font></a>
+        <form class="form-inline my-2 my-lg-0">
+          <a href="/User"><img src="{{URL ('img/logo.jpg')}}" width="30px" height="30px" > </a>
+        </form>
+    
   </div>
 </nav></body>
 </html>

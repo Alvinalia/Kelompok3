3a3938f96a313493f421b79fec3eb598100cef4e
Alvinalia <133779368+Alvinalia@users.noreply.github.com>
2023-06-26 09:45:35 +0700

    Update Label.blade.php

--- resources/views/Label.blade.php
+++ resources/views/Label.blade.php
@@ -1,6 +1,17 @@
 @include('navbarrr')
 @section('content')
-<h2>Daftar Lagu 2023 Slebew</h2>
+
+<h2>Label Musik Ter Hits</h2>
+
+<div class="my-3">
+    <a href="create3" class= "btn btn-dark ml-5">Add Data</a>
+</div>
+
+@if(Session::has('status'))
+    <div class="alert alert-success" role="alert">
+        {{Session::get('message')}}
+    </div>
+@endif
 <table class="table">
     <thead>
         <tr>
@@ -8,9 +19,8 @@
             <th>id </th>
             <th>nama </th>
             <th>alamat</th>
-            <th>id_penyanyi</th>
-            <th>id_musik</th>
-            <th>Penyanyi</th>
+            <th>Penyanyi Id</th>
+            <th>nama penyanyi</th>
         </tr>
     </thead>
     <tbody>
@@ -21,8 +31,17 @@
             <td>{{$data->nama}}</td>
             <td>{{$data->alamat}}</td>
             <td>{{$data->penyanyi_id}}</td>
-            <td>{{$data->id}}</td>
-            <td>{{$data->penyanyi['id']}}</td>
+            <td>{{$data->penyanyi?->nama}}</td>
+           
+            <td>
+            <a href="edit3/{{$data->id}}" class="btn btn-warning">Edit</a>
+            
+            <form class="d-inline" action="/Label/{{$data->id}}" method="post">
+                    @csrf
+                    @method('delete')
+                    <button type="submit" name="submit" class="btn btn-danger" onclick="return confirm('mau dii apuss nii??')">Delete</button>
+                </form>
+                <a href="login/{{$data->id}}" class="btn btn-info">Detail</a>
     
         </tr>
         @endforeach

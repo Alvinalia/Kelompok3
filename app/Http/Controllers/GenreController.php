63975d7fde688765ffe1014832cd0359787ab69b
Alvinalia <133779368+Alvinalia@users.noreply.github.com>
2023-06-26 10:24:37 +0700

    Update GenreController.php

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Genre;
+use Illuminate\Support\Facades\Session;
+use App\Models\GenreMusik;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class GenreController extends Controller
 
@@ -15,5 +20,45 @@ class GenreController extends Controller
         $GenreMusik = GenreMusik::with(['genre'])->get();
         return view('GenreMusik',['listGenreMusik' => $GenreMusik]);
     }
+
+    public function create(){
+        $Genre = Genre::all();
+        $GenreMusik = GenreMusik::all();
+        return view('create5', compact('Genre','GenreMusik'));
+    } 
+    public function store(Request $request)
+    {
+
+        $Genre = Genre::create($request->all());
+        return redirect('/Genre');
+    
+    }
+
+    public function edit5(Request $request, $id) {
+        $Genre = Genre::findOrFail($id);
+        $GenreMusik = GenreMusik::findOrFail($id);
+        return view('edit5',['listGenre' => $Genre, 'listGenreMusik' => $GenreMusik]);
+
+    }
+
+    public function update(Request $request,$id) {
+        $Genre = Genre::findOrFail($id);
+        $Genre->update($request->all());
+        return redirect('/Genre');
+
+    }
+
+    public function destroy($id)
+    {
+        $Genre = Genre::find($id)->delete();
+
+        if($Genre){
+            Session::flash('status','success');
+            Session::flash('message','Data Berhasil Dihapus');
+        }
+        return redirect('/Genre');
+    }
+
+
 
 }
